f740bb7ac0e44caafd5e60ac4328d8da776d5f7c
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-15 10:30:15 -0600

    initial commit

---
+++ lib/game_board.rb
@@ -0,0 +1,13 @@
+class GameBoard
+#this loops until
+
+#prints empty game board 6 rows under A-G columns
+#determines where the players piece lands
+#remembers where the pieces are. Meybe create an empty array for the turns which can ba called by the index number
+#prints out the board with pieces placed between each turn
+#determines win by horizontal, vertical, diagonal or if board is full.
+
+#once one of the above conditionals is met, the loop ends
+
+
+end
---
+++ test/game_board_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/emoji'
+require './lib/game_board'
+require 'pry'
+
+class GameBoardTest < Minitest::Test
+  def test_it_exists
+    gameboard = GameBoard.new
+
+    assert_instance_of GameBoard, gameboard
+  end
+
+
+end
