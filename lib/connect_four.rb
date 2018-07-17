f34f69dc73e91ef5f2a67a6cd47c433ca33eb606
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-16 18:25:08 -0600

    adds user input phrase

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,8 +1,14 @@
 #runner file for gameplay
 require './lib/game_board.rb'
 
+round = GameBoard.new
+
 puts "Let's play a game of Connect Four"
 
 #print out empty board
-round = GameBoard.new
+
 round.game_board
+
+puts "Where would you like to place your piece?"
+guess = gets.chomp
+p guess
