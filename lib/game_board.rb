4158f670d3931a043216ab2ea6a1344c71735a32
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-16 18:56:02 -0600

    small edits

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -15,13 +15,16 @@ class GameBoard
   end
 
 #create method that prints board
-#use .transpose...need board to be array
+#use .transpose...need board to be array. makes array of array into rows and columns
+#use map to split column headers
+#join to make into one string. Add a space in between each header
 
 def game_board
     printable_board = @emptyboard.transpose
-    printable_board.each do |column|
-      puts column.map { |c| c }.join(" ")
+    printable_board.map do |header|
+      puts header.map { |c| c }.join(" ")
     end
+
 end
 
 end
