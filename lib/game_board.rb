f2ea41da115905b06c90c7a0a031003a3fe5c35b
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-16 18:06:11 -0600

    completes iteration 1 with welcome message and empty game board

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,15 +1,27 @@
+require 'pry'
 class GameBoard
-  attr_accessor :columns
+  attr_reader :emptyboard
 #build empty board for game play
   def initialize
-    @emptyboard = {
-                "A" => [".", ".", ".", ".", ".", "."],
-                "B" => [".", ".", ".", ".", ".", "."],
-                "C" => [".", ".", ".", ".", ".", "."],
-                "D" => [".", ".", ".", ".", ".", "."],
-                "E" => [".", ".", ".", ".", ".", "."],
-                "F" => [".", ".", ".", ".", ".", "."],
-                "G" => [".", ".", ".", ".", ".", "."]
-            }
+     @emptyboard =    [
+                 ["A", ".", ".", ".", ".", ".", "."],
+                 ["B", ".", ".", ".", ".", ".", "."],
+                 ["C", ".", ".", ".", ".", ".", "."],
+                 ["D", ".", ".", ".", ".", ".", "."],
+                 ["E", ".", ".", ".", ".", ".", "."],
+                 ["F", ".", ".", ".", ".", ".", "."],
+                 ["G", ".", ".", ".", ".", ".", "."]
+               ]
   end
+
+#create method that prints board
+#use .transpose...need board to be array
+
+def game_board
+    printable_board = @emptyboard.transpose
+    printable_board.each do |column|
+      puts column.map { |p| p }.join(" ")
+    end
+end
+
 end
